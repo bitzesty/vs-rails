fda6adf59b0a2506fe02664b6f38a5e7f0071287
Mateusz Kozak <mateusz@mkozak.pl>
2015-03-20 14:45:10 +0100

    Model and controller for scans

---
+++ app/controllers/vs_rails/scans_controller.rb
@@ -0,0 +1,16 @@
+class ScansController < ApplicationController
+  def callback
+    scan = Scan.where(uuid: params[:uuid])
+
+    if params[:status] == "ok"
+      scan.update_attributes(:status, "ok")
+      flash[:notice] = "File is OK"
+    elsif params[:status] == "infected"
+      scan.update_attributes(:status, "infected")
+      flash[:error] = "File is infected"
+    else
+      scan.update_attributes(:status, "unknown")
+      flash[:alert] = "File status unknown"
+    end
+  end
+end
