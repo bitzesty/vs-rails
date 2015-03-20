8c55eff151bd9d52c1201aa5862b5f141c2f71a9
Mateusz Kozak <mateusz@mkozak.pl>
2015-03-20 11:26:28 +0100

    Migration to create scans table

---
+++ db/migrate/20150320102417_create_scans.rb
@@ -0,0 +1,10 @@
+class CreateScans < ActiveRecord::Migration
+  def change
+    create_table :scans do |t|
+      t.string :filename
+      t.string :status
+
+      t.timestamps
+    end
+  end
+end
