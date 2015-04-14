cacf811cd0a0c43f446dc11d220fec59c2d80396
Matthew Ford <matt@bitzesty.com>
2015-04-14 18:56:23 +0100

    TODO: move to enum for the status

--- app/models/scan.rb
+++ app/models/scan.rb
@@ -2,6 +2,9 @@ class Scan < ActiveRecord::Base
   validates :uuid, :filename, presence: true
   validates :uuid, uniqueness: true
 
+  #TODO Move to enum
+  # enum status: %w(scanning clean infected error unknown)
+
   def clean?
     self.status == "clean"
   end
